9f6e8e125ef260e3253e0df98b6ef97808d68659
Debabi Ala <73254102+ala2020-cyber@users.noreply.github.com>
2022-10-18 09:21:43 +0200

    commit
    Adding crud to our node API .

--- server/app.js
+++ server/app.js
@@ -7,6 +7,11 @@ const {ObjectId} =require('mongodb')
 
 const app= express()
 
+//middlewares
+
+app.use(express.json())
+
+
 // database connection 
 let db
 connectToDb((err) => {
@@ -34,11 +39,44 @@ app.get("/api/movies/:id", (req,res) => {
    if(ObjectId.isValid(id)){
     db.collection('movies').findOne({_id: ObjectId(id)})
       .then((movie) => res.status(200).json({success: true, data: movie}))
-      .catch(() => res.status(404).json({success:false,error: "could not find the movie"}))
+      .catch(() => res.status(500).json({success:false,error: "could not find the movie"}))
+   }
+   else {
+    res.status(404).json({success: false, message: "Id of movie is not valid"})
    }
    
 })
+app.post("/api/movies", (req,res) => {
+    const newMovie= req.body
 
+    db.collection('movies').insertOne(newMovie)
+      .then( result => res.status(200).json({success: true, response: result}) )
+      .catch( () => res.status(500).json({success: false, error: "could not insert data on your db"}))
+})
+
+app.delete("/api/movies/:id", (req,res) => {
+    if (ObjectId.isValid(req.params.id)){
+        db.collection('movies').deleteOne({_id:ObjectId(req.params.id)})
+          .then( movie => res.status(200).json({success: true, response: movie}))
+          .catch( () => res.status(500).json({success: false, error:"could not delete the movie"}))
+    }
+    else{
+        res.status(404).json({success: false, message: "Id of movie is not valid"})
+    }
+})
+
+app.patch("/api/movies/:id", (req,res) => {
+
+    const updates= req.body
+    if (ObjectId.isValid(req.params.id)){
+        db.collection('movies').updateOne({_id:ObjectId(req.params.id)}, {$set: updates})
+          .then( result => res.status(200).json({success: true, response: result}))
+          .catch( () => res.status(500).json({success: false, error:"could not update the movie"}))
+    }
+    else{
+        res.status(404).json({success: false, message: "Id of movie is not valid"})
+    }
+})
 
 
 
