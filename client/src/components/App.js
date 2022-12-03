6ad740996fddef92a3629015f24750e02119550f
Debabi Ala <73254102+ala2020-cyber@users.noreply.github.com>
2022-12-03 12:53:20 +0100

    Update App.js

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,10 +1,16 @@
 
-import React from "react"
+import React, { useState } from "react"
 
 function App() {
+
+   const [ count, setCount ]=useState(0)
+
   return (
     <div className="App">
       <h1>Hello react, you are ready for fun</h1>
+      <button onClick={ () => setCount(count+1) }>
+        {count}
+      </button>
     </div>
   );
 }
